40db03097521e1208b6e9ae9e3e58b3108c64d56
James Wang <wang.james.j@gmail.com>
2025-10-17 11:31:24 -0700

    Created basic TextNode class.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextType
+from textnode import TextNode
+
+
+def main():
+    text_obj = TextNode("some text", TextType.LINK, "www.fakeurl.com")
+    print(text_obj)
+
+main()
